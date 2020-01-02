63671e4f466a8b2824cc1e18b7bee6b671b7f3ba
blessnhs <blessnhs@nate.com>
2020-01-03 00:44:57 +0900

    프로토콜 추가

--- Trunk/JG/Protocol/proto.bat
+++ Trunk/JG/Protocol/proto.bat
@@ -1,3 +1,9 @@
 protoc.exe -I=./ --csharp_out=./CSharp ./sample.proto
-protoc.exe -I=./ --cpp_out=./Cpp ./sample.proto
+protoc.exe -I=./ --csharp_out=./CSharp ./Enum.proto
+protoc.exe -I=./ --csharp_out=./CSharp ./CLI.GS.proto
+protoc.exe -I=./ --csharp_out=./CSharp ./GS.CLI.proto
 
+protoc.exe -I=./ --cpp_out=./Cpp ./sample.proto
+protoc.exe -I=./ --cpp_out=./Cpp ./Enum.proto
+protoc.exe -I=./ --cpp_out=./Cpp ./CLI.GS.proto
+protoc.exe -I=./ --cpp_out=./Cpp ./GS.CLI.proto
