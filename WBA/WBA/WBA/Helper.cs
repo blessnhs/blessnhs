80cf31637057c55096aed2b8087e21f2cbc767ad
blessnhs <blessnhs@nate.com>
2020-02-18 21:48:09 +0900

    누락 파일 추가

---
+++ WBA/WBA/WBA/Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WBA
+{
+    static class Helper
+    {
+        static public void RemoveRowGrid(Grid grid,int pos = 0)
+        {
+            var children = grid.Children.ToList();
+            foreach (var child in children)
+            {
+                int row = Grid.GetRow(child);
+                if (row >= pos)
+                {
+                    grid.Children.Remove(child);
+                }
+            }
+        }
+
+        static public void SpliteVerseText(string input, out int verse, out string text)
+        {
+            verse = 1;
+            text = "";
+
+            string[] header = input.Split(' ');
+
+            if (header.Length == 0)
+                return;
+
+            for (int i = 1; i < header.Length; i++)
+            {
+                text += header[i];
+                text += " ";
+            }
+        }
+    }
+}
