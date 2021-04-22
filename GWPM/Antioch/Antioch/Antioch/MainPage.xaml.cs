aeeb1e8f341b9a63cf879d9efce1f665e5d0f252
blessnhs <blessnhs@nate.com>
2021-04-22 18:06:41 +0900

    ANTIOCH

---
+++ GWPM/Antioch/Antioch/Antioch/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Antioch.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Antioch
+{
+    public partial class MainPage : ContentPage
+    {
+        Lobby lobby = new Lobby();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            symbol_image.Source = ImageSource.FromResource("Antioch.Image.symbol.png");
+           
+            ContentViews.Children.Add(lobby);
+        }
+    }
+}
--- GWPM/HUB/HUB/INI/INI.cpp
+++ GWPM/HUB/HUB/INI/INI.cpp
@@ -3,26 +3,25 @@
 
 VOID HubINI::Load()
 {
-	Port = GetPrivateProfileInt(("SERVER"), ("PORT"), 1982,("./Hub.ini"));
-	MaxUser = GetPrivateProfileInt(("SERVER"), ("MAXUSER"), 100,("./Hub.ini"));
+	std::string Name = "./Hub.ini";
+	Port = GetPrivateProfileInt(("SERVER"), ("PORT"), 1982, (Name.c_str()));
+	MaxUser = GetPrivateProfileInt(("SERVER"), ("MAXUSER"), 100, (Name.c_str()));
 
 
 	CHAR Default_Version[10];
-	GetPrivateProfileString(("SERVER"), ("VERSION"), ("1.0"), Default_Version, sizeof(Default_Version), ("./Hub.ini"));
+	GetPrivateProfileString(("SERVER"), ("VERSION"), ("1.0"), Default_Version, sizeof(Default_Version), (Name.c_str()));
 
 	Version = atof(Default_Version);
 
-	Default_FrontPort = GetPrivateProfileInt(("SERVER"), ("DEFAULT_FRONT_PORT"), 100,("./Hub.ini"));
+	Default_FrontPort = GetPrivateProfileInt(("SERVER"), ("DEFAULT_FRONT_PORT"), 100, (Name.c_str()));
 
-	AliveSeconds = GetPrivateProfileInt(("SERVER"), ("ALIVE_SECONDS"), 60 * 60 /*1HOURS*/, ("./Hub.ini"));
+	AliveSeconds = GetPrivateProfileInt(("SERVER"), ("ALIVE_SECONDS"), 60 * 60 /*1HOURS*/, (Name.c_str()));
 
-	GetPrivateProfileString(("SERVER"), ("DEFAULT_FRONT_IP"),("127.0.0.1"),Default_FrontIp,sizeof(Default_FrontIp),("./Hub.ini"));
+	GetPrivateProfileString(("SERVER"), ("DEFAULT_FRONT_IP"), ("127.0.0.1"), Default_FrontIp, sizeof(Default_FrontIp), (Name.c_str()));
 
-	GetPrivateProfileString(("SERVER"), ("ODBC_SYSTEM32_NAME"), ("."), ODBC_NAME, sizeof(ODBC_NAME), ("./Hub.ini"));
-	GetPrivateProfileString(("SERVER"), ("ODBC_ID"), ("."), ODBC_ID, sizeof(ODBC_ID), ("./Hub.ini"));
-	GetPrivateProfileString(("SERVER"), ("ODBC_PASSWD"), ("."), ODBC_PWD, sizeof(ODBC_PWD), ("./Hub.ini"));
+	GetPrivateProfileString(("SERVER"), ("ODBC_SYSTEM32_NAME"), ("."), ODBC_NAME, sizeof(ODBC_NAME), (Name.c_str()));
+	GetPrivateProfileString(("SERVER"), ("ODBC_ID"), ("."), ODBC_ID, sizeof(ODBC_ID), (Name.c_str()));
+	GetPrivateProfileString(("SERVER"), ("ODBC_PASSWD"), ("."), ODBC_PWD, sizeof(ODBC_PWD), (Name.c_str()));
 
-	printf("[ Hub ] Port [ %d ] MaxUser [ %d ] Alive Seconds[ %d ] Version [%f]\n",Port,MaxUser, AliveSeconds, Version);
-
-	//printf("ODBC SYSTEM32 NAME [%s] ID [%s] PWD [%s] \n", ODBC_NAME, ODBC_ID, ODBC_PWD);
+	printf("[ Hub ] Port [ %d ] MaxUser [ %d ] Alive Seconds[ %d ] Version [%f]\n", Port, MaxUser, AliveSeconds, Version);
 }
